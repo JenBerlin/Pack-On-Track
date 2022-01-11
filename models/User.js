b4823161295cc205cc8cbaf1c3194f4f24f537e8
mohamedallam13 <mohamedallam.tu@gmail.com>
2022-01-11 19:11:27 +0100

    Seed Users successfully

--- models/User.js
+++ models/User.js
@@ -2,6 +2,9 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+const { beforeCreate, beforeUpdate } = require("../server/hooks")
+
+
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
@@ -38,14 +41,8 @@ User.init(
   },
   {
     hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
+      beforeCreate,
+      beforeUpdate,
     },
     sequelize,
     timestamps: false,
