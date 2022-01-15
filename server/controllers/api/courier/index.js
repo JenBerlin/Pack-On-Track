4d9436826e395a2393555616641fd29b0a57cc4f
jitasek <jitka.moreira@gmail.com>
2022-01-15 22:59:44 +0100

    add route for get one courier

--- server/controllers/api/courier/index.js
+++ server/controllers/api/courier/index.js
@@ -18,4 +18,22 @@ router.get("/", async (req, res) => {
 
 // Get one courier
 
+router.get("/:id", async (req, res) => {
+  try {
+    const allCouriers = await Courier.findByPk(req.params.id, {
+      attributes: ["id", "courier_name"],
+    });
+    if (!allCouriers) {
+      res
+        .status(404)
+        .json({ data: "No courier with this ID in the database." });
+      return;
+    }
+    res.status(200).json(allCouriers);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error retrieving couriers data from database.");
+  }
+});
+
 module.exports = router;
