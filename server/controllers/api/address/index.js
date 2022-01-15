2f6c3241e1241ecf39640d711a2df677da158c07
jitasek <jitka.moreira@gmail.com>
2022-01-15 22:11:54 +0100

    add route for create address

--- server/controllers/api/address/index.js
+++ server/controllers/api/address/index.js
@@ -1,5 +1,37 @@
 const router = require("express").Router();
+const sequelize = require("../../../../config/connection");
 
 const { User, Shipment, Courier, Address } = require("../../../../models");
+const auth = require("../../../auth");
+
+// Create address - POST request
+
+router.post("/", async (req, res) => {
+  try {
+    const newAddress = {
+      user_id: req.session.user_id,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      street: req.body.street,
+      street_number: req.body.street_number,
+      additional_line: req.body.additional_line,
+      company: req.body.company,
+      post_number: req.body.post_number,
+      city: req.body.city,
+      country: req.body.country,
+      library_keyword: req.body.library_keyword,
+    };
+    const successAddress = await Address.create(newAddress);
+    return res.status(200).json(successAddress.get({ plain: true }));
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Failed to create address." });
+  }
+});
+
+// View(read) address - GET request
+
+// Update address - PUT request
+// Delete address - DELETE request
 
 module.exports = router;
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
-const addressRoutes = require("./address");
+const addressRoutes = require("../../controllers/api/address");
 const courierRoutes = require("./courier");
 const shipmentRoutes = require("../../controllers/api/shipment");
 const userRoutes = require("../../controllers/api/user");
 
 router.use("/user", userRoutes);
-// router.use("/address", addressRoutes);
+router.use("/address", addressRoutes);
 router.use("/shipment", shipmentRoutes);
 // router.use("/courier", courierRoutes);
 
